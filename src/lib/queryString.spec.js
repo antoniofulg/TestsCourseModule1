f7874ecd7883d42bb367daa2e078fe66c1207148
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-10-22 23:06:33 -0300

    more test cases for query strings

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -9,6 +9,29 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Antonio&profession=developer')
   })
+
+  it('should create a valid query string even whan an array is passed as value', () => {
+    const obj = {
+      name: 'Antonio',
+      abilities: ['JS', 'TDD']
+    }
+
+    expect(queryString(obj)).toBe('name=Antonio&abilities=JS,TDD')
+  })
+
+  it('should throw an error when object is passed as value', () => {
+    const obj = {
+      name: 'Antonio',
+      abilities: {
+        first: 'JS',
+        second: 'TDD'
+      }
+    }
+
+    expect(() => {
+      queryString(obj)
+    }).toThrowError()
+  })
 })
 
 // describe('Query string to object', () => {
