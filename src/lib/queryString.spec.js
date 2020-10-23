5c20b4d354e001ddb689621ee86d524869625056
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-10-23 08:52:52 -0300

    Implement, with TDD, a method to parse query string - part 2

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -37,9 +37,27 @@ describe('Object to query string', () => {
 describe('Query string to object', () => {
   it('should convert a query string to object', () => {
     const qs = 'name=Antonio&profession=developer'
+
     expect(parse(qs)).toEqual({
       name: 'Antonio',
       profession: 'developer'
+    })
+  })
+
+  it('should convert a query string of a single key-value pair to object', () => {
+    const qs = 'name=Antonio'
+
+    expect(parse(qs)).toEqual({
+      name: 'Antonio'
+    })
+  })
+
+  it('should convert a query string to an object taking care of comma separated values', () => {
+    const qs = 'name=Antonio&abilities=JS,TDD'
+
+    expect(parse(qs)).toEqual({
+      name: 'Antonio',
+      abilities: ['JS', 'TDD']
     })
   })
 })
